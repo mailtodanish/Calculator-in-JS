644a36175dcb7a5302255d2d855564ebe2871fe7
ankush <mehraankush36@gmail.com>
2022-10-08 08:40:45 +0530

    improved css

--- main.js
+++ main.js
@@ -1,60 +1,25 @@
-$(document).ready(function(){
-    
-    $(".cell").click(function(){
-      let v = $(this).text();
-      let input = $("#CalcInput").val();
-      if (v == "C") {
-        input ="";
-      }
-      else if (v != "=") {
-      if (input != ""){
-          previous_input = input[input.length -1];
-          decimal=false
-          if(v=="."){
-            // if decimal exist
-            
-            Array.from(input).forEach(element => {
-                if (element == "."){
-                  decimal = true
-                }
-                if(isNaN(element) && element != "."){
-                  decimal = false
-                }
-              }); 
-          }
-          if(!isNaN(previous_input) && decimal==false)
-          {            
-                input = `${input}${v}`;        
-          }
-          if(isNaN(previous_input) && !isNaN(v) && decimal==false)
-          {            
-                input = `${input}${v}`;        
-          }
-          if(decimal==true && v!=".")
-          {
-            input = `${input}${v}`; 
-          }
+// 
 
-      }else{
-        input = v;
-      }  
+
+var outputScreen=document.getElementById('inputscreen');
+
+function display(num){
+    outputScreen.value = outputScreen.value+num;
+}
+
+function calculate(){
+    try{
+        outputScreen.value=eval(outputScreen.value);
     }
-    else
-    {
-       input = eval(input.replace("x","*")) ;
-    }    
-       
-     $("#CalcInput").val(input);
-    });
+    catch(err){
+        alert("Invalid");
+    }
+}
 
-    $(".cell").on({
-        mouseenter: function(){
-            $(this).addClass();
-            $(this).removeClass();
-        },
-        mouseleave: function(){
-            $(this).addClass();
-            $(this).removeClass();
-        }
-    });
-  });
+function pressAllClear() {
+    outputScreen.value=outputScreen.value.slice(0,-15);
+    // outputScreen.innerText='';
+  }
+function del(){
+    outputScreen.value=outputScreen.value.slice(0,-1);
+}
